4b03397f57140714c250a6a6567142352a1d227a
dragon-yuan <plu_huajuan@163.com>
2017-11-07 22:58:17 +0800

    Add RedisUtils For Serial.

--- src/main/source/me/dragon/restcontroller/TestController.java
+++ src/main/source/me/dragon/restcontroller/TestController.java
@@ -1,12 +1,15 @@
 package me.dragon.restcontroller;
 
 import io.swagger.annotations.Api;
+import me.dragon.utils.RedisUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
 
 /**
  * Created by dragon on 11/4/2017.
@@ -18,9 +21,18 @@ public class TestController extends BaseController {
 
     protected Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @RequestMapping(value = "/test", method = RequestMethod.POST)
+    @Resource
+    private RedisUtils redisUtils;
+
+    @RequestMapping(value = "/testController", method = RequestMethod.POST)
     @ResponseBody
-    public void updateContact() {
-        logger.info("test接口...");
+    public void testController() {
+        logger.info("testController接口...");
+    }
+
+    @RequestMapping(value = "/testRedis", method = RequestMethod.POST)
+    @ResponseBody
+    public void testRedis() {
+        logger.info("testRedis接口..." + redisUtils.getNowCode("TEST", "T", 3));
     }
 }
